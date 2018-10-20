5275f4c8c213fbe1e2e833ceb07cd4b2ca2bdfa0
Gamaliel Petralba <petralbagamaliel@gmail.com>
2018-10-20 15:30:42 +0800

    implement selection sort

---
+++ selection-sort.py
@@ -0,0 +1,20 @@
+def select_min(sequence):
+    min = sequence[0]
+    for i in range(1, len(sequence)):
+        n = sequence[i]
+        if n < min:
+            min = n
+    return min
+
+
+def selection_sort(sequence):
+    sorted_sequence = []
+    while len(sequence) != 0:
+        min = select_min(sequence)
+        sorted_sequence.append(min)
+        sequence.remove(min)
+    print(sorted_sequence)
+
+
+sequence = [5, 8, 3, 7, 1, 9, 11, 4, 2]
+selection_sort(sequence)
